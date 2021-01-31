034dc2f237dba244151fbe2be80bde4914972fb4
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-01-31 11:51:37 +0200

    added formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    form: FormGroup;
+
+    ngOnInit() {
+        this.form = new FormGroup({})
+    }
+
+    submit() {
+        console.log("This form submited: ", this.form);
+        
+    }
 }
